7c7bb7837d14d959f0499d0b50b974b4508e7154
Harsh Dubey <147268578+harshdubey7@users.noreply.github.com>
2025-01-15 09:49:47 +0000

    comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,5 +1,5 @@
 //very important topic
-let score = undefined
+//let score = undefined
 
 // console.log(typeof score)
 // console.log(typeof(score)); //ye karne ke baad dono ke liye number print hoga 
@@ -7,7 +7,7 @@ let score = undefined
 //now convert it to strings and check
 
 //now conversion of this string to number
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);     //here it has printed 33 but instead of 33 if you would have taken string as 33abc then it would have printed NaN(Not a number)
 
 // console.log(valueInNumber);
@@ -25,15 +25,63 @@ let valueInNumber = Number(score)
 //undefined=>undefined
 //true=>1; false=>0
 
-let isLoggedIn="harsh"
+//let isLoggedIn="harsh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); //give true when 1 false when 0,empty string ke liye false
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); //give true when 1 false when 0,empty string ke liye false
                                 //with any otjher string gives true
  
 //conversion to string
-let someNumber=5
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
-                                
+// let someNumber=5
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+//OPERATIONS
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  //this is 2 to the power 2
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2= " world"
+let str3=str1+str2
+console.log(str3);
+
+//some complex situations
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");   //#note: agar string pehle hai to sabko string ki tarah treat kiya jaayega ang agar string last me hai to pehle jo operation hai wo ho jayega and then string waala aayega
+
+//some more tricky conversions
+
+console.log(+true);  //true+ ke liye error aa jaayega
+
+let num1,num2,num3
+
+num1=num2=num3=2+2 //dont prefer writing code like this
+
+let gameCounter =100
+console.log(gameCounter++);
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+//console.log(2>1);
+//comparison of different datatypes
+//console.log("2">1); //but thios is not a good practice ,make sure to compare same datatypes
+console.log(null>0); //false
+console.log(null<0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+//jab comparison hota hai tab us time pe equality check and comparison(>,<,>=,<=) dono alag tareeke se kaam karte hai
+//comparison null ko ek number me convert kar deta hai, treating it as 0.
+//isilye null>=0 is true
+
+//comparison of undefined
+
+console.log(undefined==0)
+console.log(undefined>0);
+console.log(undefined<0);//saara false hi aayega
+
+//comparison with ===
+//=== does the comparison along with checking the datatype as well
+
+console.log("2"===2); //false ,checked the value alongwith datatypes
+
+//TRY WRITING THE CLEAN CODES 
+
+
+    
